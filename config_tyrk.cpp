fc632d496b5709b9b0aa6fc0827de3179a687c72
Kuchenplatte <40145065+Kuchenplatte@users.noreply.github.com>
2019-07-21 20:49:51 +0200

    Add files via upload
    TRYK Classname

---
+++ config_tyrk.cpp
@@ -0,0 +1,180 @@
+	///////////////////////////////////////////////////////////////////////////////
+	// TRYK KLEIDUNG
+	///////////////////////////////////////////////////////////////////////////////
+	class TRYK_B_TRYK_3C_T		 						{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_V_Bulletproof_BLK			 			{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_ACUTshirt			 				{ quality = 3; price = 1500; sellPrice = 550;};
+	class TRYK_U_B_ARO1_CombatUniform 					{ quality = 3; price = 1500; sellPrice = 450;}; 
+	class TRYK_U_B_ARO1R_CombatUniform		 			{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_ARO1_BLK_CombatUniform 				{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_ARO1_BLK_R_CombatUniform 			{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_ARO1_CBR_CombatUniform 				{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_ARO1_GR_CombatUniform 				{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_ARO1_GR_R_CombatUniform 				{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_ARO1_GRY_CombatUniform 				{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_ARO1_GRY_R_CombatUniform 			{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_ARO2_CombatUniform 					{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_ARO2R_CombatUniform 					{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_DMARPAT_T								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_C_AOR2_T 								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_AOR2_BLK_CombatUniform 				{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_AOR2_BLK_R_CombatUniform				{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_AOR2_GRY_CombatUniform 				{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_AOR2_GRY_R_CombatUniform				{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_PCUHsW8 								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_PCUHsW3 								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_PCUHsW9								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_Bts_PCUs 								{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_PCUHsW								{ quality = 3; price = 2500; sellPrice = 600;}; // Winterjacke
+	class TRYK_U_B_PCUHsW6 								{ quality = 3; price = 2500; sellPrice = 600;}; // Winterjacke
+	class TRYK_U_B_PCUHsW4 								{ quality = 3; price = 2500; sellPrice = 600;}; //Winterjacke
+	class TRYK_U_Bts_PCUGs								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_Bts_GRYGRY_PCUs 						{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_PCUHsW7								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_BLKBLK_CombatUniform 				{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_BLKBLK_R_CombatUniform 				{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_BLKOCP_CombatUniform					{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_BLOD_T 								{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_BLKTAN_CombatUniform					{ quality = 3; price = 1500; sellPrice = 600;};  
+	class TRYK_U_B_BLKTANR_CombatUniformTshirt 			{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_BLTAN_T								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_BLK_Tshirt 							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_C01_Tsirt							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_3CD_Delta_BDUTshirt 					{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_3CD_BLK_BDUTshirt 					{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_3CD_BLK_BDUTshirt2					{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_C02_Tsirt 							{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_BLK3CD_Tshirt						{ quality = 3; price = 1500; sellPrice = 600;};  
+	class TRYK_U_B_BLK3CD 								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_BLKTAN_Tshirt						{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_BLKTAN 								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_BLK_OD_Tshirt						{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_OD_BLK 								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_OD_BLK_2 							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_ODTAN								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_ODTAN_Tshirt 						{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_3CD_Delta_BDU						{ quality = 3; price = 1500; sellPrice = 600;};  
+	class TRYK_U_B_fleece 								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_fleece_UCP							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_GRTAN_CombatUniform 					{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_GRTANR_CombatUniformTshirt			{ quality = 3; price = 1500; sellPrice = 600;};  
+	class TRYK_U_B_GRYOCP_CombatUniform 				{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_GRYOCP_R_CombatUniformTshirt			{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_OVERALL_SAGE_BLKboots_nk_blk2 			{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_OVERALL_nok_flesh						{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_MARPAT_Desert_Tshirt 				{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_MARPAT_Wood_Tshirt 					{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_MARPAT_WOOD_CombatUniform			{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_MTP_BLK_CombatUniform 				{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_MTP_BLK_R_CombatUniform				{ quality = 3; price = 1500; sellPrice = 600;};  
+	class TRYK_U_B_Denim_T_WH 							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_Denim_T_BK							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_Denim_T_BG_WH 						{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_Denim_T_BG_BK						{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_pad_j_blk 								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_denim_hood_nc 							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_denim_hood_blk							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_denim_hood_3c 							{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_Snowt								{ quality = 3; price = 4500; sellPrice = 600;};  //Snow
+	class TRYK_U_B_Snow_CombatUniform 					{ quality = 3; price = 4500; sellPrice = 600;}; // Snow
+	class TRYK_shirts_TAN_PAD_YEL 						{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_shirts_BLK_PAD_YEL						{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_shirts_TAN_PAD_RED2 						{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_shirts_BLK_PAD_RED2						{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_shirts_TAN_PAD_BLU3 						{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_shirts_DENIM_RED2_Sleeve 				{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_shirts_DENIM_BWH_Sleeve					{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_shirts_DENIM_BK_Sleeve 					{ quality = 3; price = 1500; sellPrice = 600;}; 
+	class TRYK_U_B_PCUGs_OD								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_PCUGs_gry 							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_Wood_PCUs							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_UCP_PCUs 							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_PCUGs_BLK							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_PCUGs_OD_R 							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_PCUGs_BLK_R							{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_PCUODHs 								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_U_B_PCUGHs 								{ quality = 3; price = 1500; sellPrice = 600;};
+	class TRYK_ZARATAKI3								{ quality = 3; price = 2500; sellPrice = 600;}; // Taliban
+	class TRYK_ZARATAKI2 								{ quality = 3; price = 2500; sellPrice = 600;}; // Taliban
+	class TRYK_ZARATAKI 								{ quality = 3; price = 2500; sellPrice = 600;}; // Taliban
+	class TRYK_SUITS_BR_F 								{ quality = 3; price = 2500; sellPrice = 600;}; // Taliban
+	class TRYK_SUITS_BLK_F 								{ quality = 3; price = 2500; sellPrice = 600;}; // Taliban
+	
+	
+	
+	
+	///////////////////////////////////////////////////////////////////////////////
+	// WESTEN
+	///////////////////////////////////////////////////////////////////////////////
+	class TRYK_V_ArmorVest_AOR2_2 						{ quality = 26; price = 15000; sellPrice = 4500;}; // teuer machen hat viel ballistik // 15000
+	class TRYK_V_ArmorVest_Brown2		 				{ quality = 26; price = 15000; sellPrice = 2500;}; // teuer machen hat viel ballistik // 15000
+	class TRYK_V_ArmorVest_cbr2 						{ quality = 26; price = 15000; sellPrice = 2500;}; // teuer machen hat viel ballistik // 15000
+	class TRYK_V_ArmorVest_khk2 						{ quality = 26; price = 15000; sellPrice = 2500;}; // teuer machen hat viel ballistik // 15000
+	class TRYK_V_ArmorVest_AOR1_2 						{ quality = 26; price = 15000; sellPrice = 2500;}; // teuer machen hat viel ballistik // 15000
+	class TRYK_V_ArmorVest_coyo2 						{ quality = 26; price = 15000; sellPrice = 2500;}; // teuer machen hat viel ballistik // 15000
+	class TRYK_V_ArmorVest_Delta2 						{ quality = 26; price = 15000; sellPrice = 2500;}; // teuer machen hat viel ballistik // 15000
+	class TRYK_V_ArmorVest_green2 						{ quality = 26; price = 15000; sellPrice = 2500;}; // teuer machen hat viel ballistik // 15000
+	class TRYK_V_ArmorVest_Ranger2 						{ quality = 26; price = 15000; sellPrice = 2500;}; // teuer machen hat viel ballistik // 15000
+	class TRYK_V_ArmorVest_tan2 						{ quality = 26; price = 15000; sellPrice = 2500;}; // teuer machen hat viel ballistik // 15000
+	class TRYK_V_PlateCarrier_coyo 						{ quality = 3; price = 1000; sellPrice = 300;};
+	class TRYK_V_PlateCarrier_ACU						{ quality = 3; price = 1000; sellPrice = 300;};
+	class TRYK_V_PlateCarrier_wood 						{ quality = 3; price = 1000; sellPrice = 300;};
+	class TRYK_V_PlateCarrier_coyo_L 					{ quality = 3; price = 1000; sellPrice = 300;};
+	class TRYK_V_PlateCarrier_ACU_L						{ quality = 3; price = 1000; sellPrice = 300;};
+	class TRYK_V_PlateCarrier_blk 						{ quality = 3; price = 1000; sellPrice = 300;}; 
+	class TRYK_V_PlateCarrier_JSDF						{ quality = 3; price = 1000; sellPrice = 300;}; 
+	class TRYK_V_PlateCarrier_blk_L 					{ quality = 3; price = 1000; sellPrice = 300;};
+	class TRYK_V_PlateCarrier_wood_L 					{ quality = 3; price = 1000; sellPrice = 300;};
+	class TRYK_V_Bulletproof_BL 						{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_U_B_ACU			 						{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_Bulletproof			 				{ quality = 10; price = 8000; sellPrice = 2500;};
+	class TRYK_V_ArmorVest_AOR1 						{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_ArmorVest_AOR2							{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_ArmorVest_Brown 						{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_ArmorVest_CBR 							{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_ArmorVest_coyo							{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_ArmorVest_Delta 						{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_ArmorVest_green						{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_ArmorVest_khk 							{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_ArmorVest_Ranger 						{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_ArmorVest_tan							{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_IOTV_BLK 								{ quality = 10; price = 8000; sellPrice = 3500;}; 
+	class TRYK_V_PlateCarrier_JSDF						{ quality = 10; price = 8000; sellPrice = 3500;}; 
+	class TRYK_V_ArmorVest_Brown 						{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_ArmorVest_CBR 							{ quality = 10; price = 8000; sellPrice = 3500;};
+	class TRYK_V_ArmorVest_coyo							{ quality = 10; price = 8000; sellPrice = 3500;};
+	
+	///////////////////////////////////////////////////////////////////////////////
+	// TRYK BACKPACKS
+	///////////////////////////////////////////////////////////////////////////////
+	class TRYK_B_Alicepack								{ quality = 10; price = 9500; sellPrice = 1500;};
+	class TRYK_B_Coyotebackpack_BLK 					{ quality = 10; price = 9500; sellPrice = 1500;};
+	class TRYK_B_Coyotebackpack							{ quality = 10; price = 9500; sellPrice = 1500;}; //Teuer machen
+	class TRYK_B_Coyotebackpack_OD 						{ quality = 10; price = 9500; sellPrice = 1500;}; //Teuer machen
+	class TRYK_B_Coyotebackpack_WH 						{ quality = 10; price = 9500; sellPrice = 1500;}; //Teuer machen
+	class TRYK_B_Medbag_BK								{ quality = 10; price = 9500; sellPrice = 1500;};
+	class TRYK_B_BAF_BAG_BLK 							{ quality = 10; price = 9500; sellPrice = 1500;};
+	class TRYK_B_tube_blk								{ quality = 3; price = 1500; sellPrice = 500;};
+	
+
+	
+
+	
+	///////////////////////////////////////////////////////////////////////////////
+	// TRYK Balaclava
+	///////////////////////////////////////////////////////////////////////////////
+	class TRYK_kio_balaclava_BLK						{ quality = 15; price = 2500; sellPrice = 500;};
+	class TRYK_kio_balaclavas 							{ quality = 15; price = 2500; sellPrice = 500;};
+	class TRYK_kio_balaclava_WH							{ quality = 15; price = 2500; sellPrice = 500;};
+	class TRYK_kio_balaclava_BLK_ear 					{ quality = 15; price = 2500; sellPrice = 500;};
+	class TRYK_kio_balaclava_ear						{ quality = 15; price = 2500; sellPrice = 500;};
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
